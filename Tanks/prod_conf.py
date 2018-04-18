4b85ba7ffa58671b7c4d873939adf8fd57dfb645
Dimitar Tasev <dimtasev@gmail.com>
2018-04-18 11:21:47 +0100

    Added empty production config

---
+++ Tanks/prod_conf.py
@@ -0,0 +1,14 @@
+# generate a new secret key
+SECRET_KEY = ""
+
+# set up PostgreSQL database connection
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
